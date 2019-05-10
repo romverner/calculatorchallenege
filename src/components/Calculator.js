2d0635552004b0cae4a7626bd33478ad97e4afdf
Roman Verner <romverj@gmail.com>
2019-05-09 21:18:39 -0400

    finishing up

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,7 +13,8 @@ class Calculator extends Component {
             tax: 0.00,
             total: 0.00,
             tip: 15.00,
-            tipPay: 0
+            tipPay: 0,
+            taxPay: 0,
         };
     };
 
@@ -50,20 +51,19 @@ class Calculator extends Component {
         let tax = this.state.tax;
         let total = this.state.total;
         let tipPercent = this.state.tip / 100;
-
         let tipTotal = 0.00;
-        let pay = 0;
+        let taxTotal = 0.00;
+        let pay = 0.00;
 
         tipTotal = ((total - tax) * tipPercent);
-        console.log(tipTotal);
         pay = (parseInt(total) + parseInt(tipTotal)) / people;
-        console.log(pay);
         tipTotal = tipTotal / people;
-        console.log(tipTotal);
+        taxTotal = tax / people;
 
         this.setState({
             pay: pay,
-            tipTotal: tipTotal
+            tipPay: tipTotal,
+            taxPay: taxTotal
         });
     };
     
@@ -75,23 +75,23 @@ class Calculator extends Component {
                     <div className="row mb-3">
                         <div className="col-12">
                             <h1>You Pay:</h1>
-                            <h1>${this.state.pay.toFixed(2)} </h1><span style={topStyle}>of which ${this.state.tipPay.toFixed(2)} is tip.</span>
+                            <h1>${this.state.pay.toFixed(2)} </h1><span style={topStyle}>of which ${this.state.tipPay.toFixed(2)} is <b>tip</b> and ${this.state.taxPay.toFixed(2)} is <b>tax</b>.</span>
                         </div>
                     </div>
                     <div className="row mb-3 bg-light pt-2">
-                        <div className="col-3 text-center">
+                        <div className="col-4 text-center">
                             <h5>Total:</h5>
                             <h4>${this.state.total}</h4>
                         </div>
-                        <div className="col-3">
+                        {/* <div className="col-3">
                             <h5>Tax:</h5>
                             <h4>${this.state.tax}</h4>
-                        </div>
-                        <div className="col-3">
+                        </div> */}
+                        <div className="col-4">
                             <h5>People:</h5>
                             <h4>{this.state.people}</h4>
                         </div>
-                        <div className="col-3">
+                        <div className="col-4">
                             <h5>Tip:</h5>
                             <h4>{this.state.tip}%</h4>
                         </div>
@@ -117,7 +117,7 @@ class Calculator extends Component {
                             <h4>Tip:</h4>
                         </div>
                         <div className="col-8 text-left">
-                            <input className="input-group form-control" type="text" name="total" maxLength="8" placeholder="Enter desired percent tip." onChange={this.tipTracker}></input>
+                            <input className="input-group form-control" type="text" name="total" maxLength="2" placeholder="Enter desired percent tip." onChange={this.tipTracker}></input>
                         </div>
                     </div>
                     <div className="row mt-4">
@@ -125,7 +125,7 @@ class Calculator extends Component {
                             <h4>Tax:</h4>
                         </div>
                         <div className="col-8 text-left">
-                            <input className="input-group form-control" type="text" name="total" maxLength="2" placeholder="Enter the total tax shown on your bill." onChange={this.taxTracker}></input>
+                            <input className="input-group form-control" type="text" name="total" maxLength="8" placeholder="Enter the total tax shown on your bill." onChange={this.taxTracker}></input>
                         </div>
                     </div>
                     <div className="row mt-4 pb-3">
