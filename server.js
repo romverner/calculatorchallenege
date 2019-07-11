757683a5330340004cc3cc098715f103baad2b7d
Roman Verner <romverj@gmail.com>
2019-07-11 19:41:26 -0400

    server update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+app.use('/static', express.static(path.join(__dirname, 'client/build')));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
